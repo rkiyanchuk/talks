17762f3b164a69ad309d199a49f417b8d1b37d0d
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2024-05-12 13:45:40 +0200

    Add GCM demo

--- pycon-italy-2024/app.py
+++ pycon-italy-2024/app.py
@@ -6,7 +6,7 @@ from utils import read_image, write_image, tamper
 
 # Encrypt with ECB
 
-key = os.urandom(32)
+key = os.urandom(256 // 8)
 iv = os.urandom(16)
 cipher = Cipher(algorithms.AES256(key), modes.ECB())
 
@@ -63,3 +63,25 @@ padded_data = decryptor.update(data) + decryptor.finalize()
 plaintext = unpadder.update(padded_data)
 
 write_image('test.bmp', meta, plaintext)
+
+
+# Encrypt with GCM
+
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+key = AESGCM.generate_key(bit_length=256)
+aes_gcm = AESGCM(key)
+nonce = os.urandom(12)
+nonce2 = os.urandom(12)
+
+meta, data = read_image('data.bmp')
+ciphertext1 = aes_gcm.encrypt(nonce, data, None)
+write_image('encrypted-aes-gcm-1.bmp', meta, ciphertext)
+
+meta, data = read_image('evil.bmp')
+ciphertext2 = aes_gcm.encrypt(nonce2, data, None)
+write_image('encrypted-aes-gcm-2.bmp', meta, ciphertext)
+
+xor = bytearray([ciphertext1[i] ^ ciphertext2[i] for i in range(len(ciphertext1))])
+write_image('encrypted-aes-gcm-nonce-reuse.bmp', meta, xor)
