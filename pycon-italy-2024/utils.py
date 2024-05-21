0ef4d0243b347ff851fdd5a6f27715c92a9c53e6
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2024-05-22 00:50:58 +0200

    Cleanup

--- pycon-italy-2024/utils.py
+++ pycon-italy-2024/utils.py
@@ -1,24 +1,3 @@
-def read_image(name):
-    with open(name, "rb") as f:
-        image = bytearray(f.read())
-    offset = image[10]
-    header, data = image[0:offset], image[offset:]
-    return header, data
-
-
-def write_image(name, header, data):
-    with open(name, "wb") as f:
-        f.write(header + data)
-
-
-def tamper(inject_name, target_name):
-    _, src_data = read_image(inject_name)
-    meta, data = read_image(target_name)
-    for i in range(min(len(data), len(src_data))):
-        data[i] = data[i] ^ src_data[i]
-    write_image(target_name, meta, data)
-
-
 class Image:
     def __init__(self, fname):
         with open(fname, "rb") as f:
